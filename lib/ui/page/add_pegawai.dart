72291735c3cfb515926655b355a52261503d8d73
triyono <yono.tri@gmail.com>
2020-10-12 22:57:19 +0700

    add button

--- lib/ui/page/add_pegawai.dart
+++ lib/ui/page/add_pegawai.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_gaji_pegawai/controllers/pegawai_controllers.dart';
 import 'package:aplikasi_gaji_pegawai/ui/widgets/template_form_field.dart';
 import 'package:flutter/material.dart';
 
@@ -8,27 +9,68 @@ class AddPegawaiPage extends StatefulWidget {
 }
 
 class _AddPegawaiPageState extends State<AddPegawaiPage> {
+  TextEditingController namaController = TextEditingController();
+  TextEditingController gajiController = TextEditingController();
+  TextEditingController umurController = TextEditingController();
+
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Add Pegawai'),
       ),
-      body: Column(
-        children: [
-          TemplateTextFormField(
-            icon: Icons.person,
-            label: 'Nama',
-          ),
-          TemplateTextFormField(
-            icon: Icons.cake,
-            label: 'Umur',
-          ),
-          TemplateTextFormField(
-            icon: Icons.monetization_on,
-            label: 'Gaji',
-          ),
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TemplateTextFormField(
+              keyboardType: TextInputType.text,
+              textEditingController: namaController,
+              icon: Icons.person,
+              label: 'Nama',
+            ),
+            TemplateTextFormField(
+              keyboardType: TextInputType.number,
+              textEditingController: umurController,
+              icon: Icons.cake,
+              label: 'Umur',
+            ),
+            TemplateTextFormField(
+              keyboardType: TextInputType.number,
+              textEditingController: gajiController,
+              icon: Icons.monetization_on,
+              label: 'Gaji',
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  PegawaiController()
+                      .addPegawai(
+                    nama: namaController.text,
+                    gaji: gajiController.text,
+                    umur: umurController.text,
+                  )
+                      .then((value) {
+                    value == true
+                        ? Navigator.of(context).pop(true)
+                        : _scaffoldKey.currentState.showSnackBar(
+                            SnackBar(content: Text('Gagal Add Pegawai')));
+                  });
+                }
+              },
+              child: Text('Add Pegawai'),
+            ),
+          ],
+        ),
       ),
     );
   }
