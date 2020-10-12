400d0844b3ede59d8b9fa6e2aa0242ae26145bf1
triyono <yono.tri@gmail.com>
2020-10-12 21:13:44 +0700

    add utils

---
+++ lib/utils/utils.dart
@@ -0,0 +1,7 @@
+class Utils {
+  static const String baseUrl = 'http://employee-crud-flutter.daengweb.id';
+  static const String listPegawai = '$baseUrl/index.php';
+  static const String addPegawai = '$baseUrl/add.php';
+  static const String updatePegawai = '$baseUrl/update.php';
+  static const String deletePegawai = '$baseUrl/delete.php';
+}
