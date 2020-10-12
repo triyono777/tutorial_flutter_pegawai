ed29fb266a5736141beaab8bc9081a288e92e820
triyono <yono.tri@gmail.com>
2020-10-12 20:17:10 +0700

    fix template

--- lib/ui/widgets/template_form_field.dart
+++ lib/ui/widgets/template_form_field.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 class TemplateTextFormField extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final TextEditingController textEditingController;
+  final TextInputType keyboardType;
+
   const TemplateTextFormField({
     Key key,
+    this.label = 'label',
+    this.icon = Icons.label,
+    this.textEditingController,
+    this.keyboardType = TextInputType.text,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: 'Nama',
-        prefixIcon: Icon(Icons.person),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: TextFormField(
+        validator: (text) {
+          if (text == null || text.isEmpty) {
+            return 'Inputan tidak boleh kosong';
+          }
+          return null;
+        },
+        controller: textEditingController,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+            labelText: label,
+            hintText: 'Masukkan $label disini',
+            prefixIcon: Icon(icon),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+            )),
       ),
     );
   }
